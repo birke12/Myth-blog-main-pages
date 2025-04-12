72cb5ce0d3805fa17f577dbb852b76e0dc1a6148
birke12 <biirkegaard@gmail.com>
2025-04-12 12:07:37 +0200

    op

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+
 import "./index.css";
 import App from "./App.jsx";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <React.StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
+</React.StrictMode>
 );
